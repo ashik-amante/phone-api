872b7be671036d62a0840a2ec39374d64aa0be0f
Abdullah Al Ashik <abdullahashik17@gmail.com>
2024-09-08 02:29:33 +0600

    update card by templet string

---
+++ js/phone.js
@@ -0,0 +1,43 @@
+const loadPhone = async () =>{
+    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+    const data =await res.json();
+    const phones = data.data
+    // console.log(phones);
+    displayPhone(phones)
+}
+
+const displayPhone = phones =>{
+    const phoneContainer = document.getElementById('phone-container');
+    phones.forEach(phone => {
+        const phoneCard = document.createElement('div');
+        phoneCard.classList = `card bg-base-100 w-96 shadow-xl`
+        phoneCard.innerHTML = `
+            <figure>
+                    <img
+                    src="${phone.image}"
+                    alt="Shoes" />
+                </figure>
+                <div class="card-body">
+                    <h2 class="card-title">${phone.phone_name}</h2>
+                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <div class="card-actions justify-end">
+                    <button class="btn btn-primary">Buy Now</button>
+                    </div>
+                </div>
+        `
+        phoneContainer.appendChild(phoneCard)
+
+        console.log(phone);
+    })
+}
+
+
+
+
+
+
+
+
+
+
+loadPhone()
